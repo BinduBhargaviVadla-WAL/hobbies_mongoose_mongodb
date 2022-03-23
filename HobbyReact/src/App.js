09a45f1a6dd0ef1a919ba43375487d048e7d6df9
BinduBhargaviVadla-WAL <bindu.v@westagilelabs.com>
2022-03-23 11:21:11 +0530

    Added files

---
+++ HobbyReact/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import HobbyServer from "./hobbyServer";
+function App() {
+  return (
+    <div className='App'>
+      <HobbyServer />
+    </div>
+  );
+}
+
+export default App;
